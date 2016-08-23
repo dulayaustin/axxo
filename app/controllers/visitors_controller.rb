bd8db959f87b6a033b6f530dea307edc9752f0cd
Austin Dulay <dulayaustin@gmail.com>
2016-08-23 14:49:26 +0800

    Latest commit

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,30 @@
+
+
 class VisitorsController < ApplicationController
+
+  def index
+    @movies = Hash.new   
+    @movies_array = []
+    (2..5).each do |page_number|    
+      doc = Nokogiri::HTML(open("http://axxomovies.org/page/#{page_number}/"))
+      doc.css("div.post").each do |post|
+        title = post.css(".post-title").text
+        link = post.at_css("a").attributes["href"].value
+        image = post.at_css("img").attributes["src"].value
+
+        @movies[title] = Hash.new
+        @movies[title][image] = link
+
+        @movies_array << @movies        
+      end
+    end
+
+    @movies_array = Kaminari.paginate_array(@movies_array).page(params[:page]).per(3)
+  end
+
+  # def display
+  #   url = params[:link]
+  #   page = Nokogiri::HTML(open(url))
+   
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :visitors, only: [:index] do
+    collection do 
+      get :display
+    end
+  end
   root to: 'visitors#index'
 end
