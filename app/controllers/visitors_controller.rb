180cc24df150869c3a387727a3e9fd6bf52f7d1e
dan rey oquindo <danreyoquindo@gmail.com>
2016-08-23 15:27:20 +0800

    changes

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,19 +1,20 @@
-
-
 class VisitorsController < ApplicationController
 
   def index
     @movies = Hash.new   
     @movies_array = []
-    (2..5).each do |page_number|    
+    (2..10).each do |page_number|    
       doc = Nokogiri::HTML(open("http://axxomovies.org/page/#{page_number}/"))
       doc.css("div.post").each do |post|
         title = post.css(".post-title").text
         link = post.at_css("a").attributes["href"].value
         image = post.at_css("img").attributes["src"].value
+        genre = post.css(".cats").children.collect { |x| x.name == "a" ? x.text : ""}.reject(&:blank?)
 
         @movies[title] = Hash.new
-        @movies[title][image] = link
+        @movies[title][:image] = image
+        @movies[title][:link] = link
+        @movies[title][:genre] = genre
 
         @movies_array << @movies        
       end
@@ -22,9 +23,9 @@ class VisitorsController < ApplicationController
     @movies_array = Kaminari.paginate_array(@movies_array).page(params[:page]).per(3)
   end
 
-  # def display
+  def display
   #   url = params[:link]
   #   page = Nokogiri::HTML(open(url))
    
-  # end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+
+  def genre_status(status)
+    if ["horror", "drama", "action", "sports"].include?(status.downcase)
+      klass = "danger"
+    elsif ["adventure", "animation", "biography", "thriller"].include?(status.downcase)
+      klass = "warning"
+    elsif ["comedy", "crime", "documentary", "war"].include?(status.downcase)
+      klass = "info"
+    elsif ["family", "fantasy", "music", "western"].include?(status.downcase)
+      klass = "success"
+    elsif ["mystery", "romance", "sci-fi"].include?(status.downcase)
+      klass = "primary"
+    else
+      klass = "default"
+    end
+
+    content_tag :div, class: "label label-#{klass}" do
+      status.titleize
+    end    
+  end
 end
